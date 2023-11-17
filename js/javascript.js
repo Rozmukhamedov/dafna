f5c864c598e6b289409788429cb9ae2e54188eaf
Rozmukhamedov Islom <workislom@gmail.comgit config --global user.name Rozmukhamedov>
2023-11-17 15:57:30 +0500

    update: 17:11:2023 15:57

--- js/javascript.js
+++ js/javascript.js
@@ -1,13 +1,60 @@
+// fixed header during scroll
+
+let headerMenu = document.getElementById("navbar-fixed");
+let headerMenuHeight = headerMenu.offsetHeight;
+
+window.addEventListener(
+  "scroll",
+  () => {
+    const top = document.documentElement.scrollTop || document.body.scrollTop;
+    if (top > headerMenuHeight) {
+      headerMenu.classList.add("fixed-top");
+    } else {
+      headerMenu.classList.remove("fixed-top");
+    }
+  },
+  false
+);
+
+// swiper slider
+
 let swiper = new Swiper(".simple-swiper", {
-  slidesPerView: 3,
-  spaceBetween: 20,
+  breakpoints: {
+    0: {
+      slidesPerView: "auto",
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  },
 });
 
 let swiperSale = new Swiper(".swiper-sale", {
-  slidesPerView: 4,
-  spaceBetween: 30,
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".swiper-button-sale-next",
+    prevEl: ".swiper-button-sale-prev",
+  },
+  breakpoints: {
+    480: {
+      freeMode: true,
+      slidesPerView: "auto",
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    992: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
   },
 });
